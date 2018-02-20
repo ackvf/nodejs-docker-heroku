e7fc0427de8fc21761dcdabfec9cb7ff201f8bd0
Vítězslav Ackermann <vitezslavferko@gmail.com>
2018-02-20 17:58:33 +0100

    View available ENV variables (such as current commit hash)

--- srcserver/index.js
+++ srcserver/index.js
@@ -9,8 +9,26 @@ const HOST = '0.0.0.0'
 // App
 const app = express()
 app.get('/', (req, res) => {
-  res.send('Hello Babel!\n')
+  res.send(`
+    Hello Babel!\n
+    <br><br>\n
+    <h3>Follows process.env</h3>\n
+      <pre>${printEnv()}</pre>
+  `)
 })
 
 app.listen(PORT, HOST)
 console.log(`Running on http://${HOST}:${PORT}`)
+
+
+function printEnv() {
+  return JSON.stringify(process.env, (k, v) => {
+
+    if (k === '') return v
+
+    if (!!k.match('(HEROKU_|npm_package_(?!(dev|dep|scripts)))')) return v
+
+    return undefined
+
+  }, ' ')
+}
